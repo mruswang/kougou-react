0c2b48295f128762cc2f8a7a16e81f941a9ee051
mruswang <875484737@qq.com>
2018-02-03 16:21:31 +0800

    'first'

---
+++ README.md
@@ -0,0 +1,1 @@
+# 接口接入文档 https://segmentfault.com/a/1190000010222913
---
+++ src/components/header/header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        这里是头部
+      </div>
+    );
+  }
+}
+
+export default Header;
